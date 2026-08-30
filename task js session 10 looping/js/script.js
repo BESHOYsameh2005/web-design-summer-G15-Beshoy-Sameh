1b0515836569e6d4da25d7dc02a4a32f619e8978
Beshoy_Sameh <beshoysameh912005@gmail.com>
2026-08-30 13:13:35 +0300

    task js session 10 looping

---
+++ task js session 10 looping/js/script.js
@@ -0,0 +1,18 @@
+function looping(startNum, endNum, contNum, breakNum) {
+
+    if (startNum != null && endNum != null && breakNum != null && contNum != null) {
+        for (var i = startNum; i <= endNum; i++) {
+            if (i == contNum) {
+                continue;
+            }
+            if (i == breakNum) {
+                break;
+            }
+            console.log(i);
+        }
+    } else {
+        alert("Please enter all values");
+    }
+}
+
+looping(1, 10, 5, 9);
